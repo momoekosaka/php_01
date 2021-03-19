ace7dd61ffc78076d33caf92276998d55f703f78
momoekosaka <80142104+momoekosaka@users.noreply.github.com>
2021-03-19 18:01:31 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -4,62 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css?v=2">
-    <title>sanrio_slot</title>
-    <h1>サンリオスロット</h1>
+    <title>Document</title>
 </head>
 <body>
-
-<!-- スロット３連 -->
-<div id = box00 >
-  <div id= box01 >
-  
-   <!-- ① -->
-   </div>
-   <div id= box02 >
-   <!-- ② -->
-   </div>
-   <div id= box03 >
-   <!-- ③ -->
-   </div>
-  </div>
- 
-
-<div id=box10>
-   <button id=btn1>One</button>
-   <button id=btn2>Two</button>
-   <button id=btn3>Three</button>
-</div> 
-  
-
 <?php
-
-   '#btn1'.click (function (){
+    
+    // おみくじアプリ
     // 必須　１画像を使う<img src =img/01.jpeg />
     //      ２if文を使う
-    //      3 mt_rand を使う
+    //      3mt_rand を使う
 
     // ランダムな数字を生成する
-    $num = mt_rand(1,4);
-
+    echo "<br>";
+    $num = mt_rand(1,3);
+    // echo $num;
+   
     // if 文を作る
     if ($num == 1){
-        echo '<img src="img/01.jpg" alt="" />';
+        echo '<img src="img/01.png" alt="" />';
       }
     if ($num == 2){
-        echo '<img src="img/02.png" alt="" />';
+        echo '<img src="img/02.jpg" alt="" />';
       }
-    if ($num == 3){
+      if ($num == 3){
         echo '<img src="img/03.jpg" alt="" />';
       }
-    if ($num == 4){
-       echo '<img src="img/04.jpg" alt="" />';
-     }
-    })
-
-    
-  
-
 
 ?>
 </body>
